56b381585cea1941c99d82118ea4f7817d3513dc
Ovidio Mallo <ovidio.mallo@netcetera.com>
2016-12-09 16:06:48 +0100

    Buh

--- deployment/pipeline.groovy
+++ deployment/pipeline.groovy
@@ -13,7 +13,7 @@ def getReplicasOrDefault(deploymentConfig, project, defaultReplicas) {
 
 @NonCPS
 def getConfig() {
-  return new Yaml(new Constructor(HashMap)).load(readFile("deployment/config.yaml"))
+  return new Yaml().loadAs(readFile("deployment/config.yaml"), HashMap)
 }
 
 node() {
