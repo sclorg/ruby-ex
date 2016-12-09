1a5effd9a542a6156d226b3c34492e02b47fd158
Ovidio Mallo <ovidio.mallo@netcetera.com>
2016-12-09 16:40:18 +0100

    Buh

--- deployment/pipeline.groovy
+++ deployment/pipeline.groovy
@@ -13,16 +13,8 @@ def getReplicasOrDefault(deploymentConfig, project, defaultReplicas) {
 
 @NonCPS
 def getConfig() {
-String exampleYaml = '''widgets:
-                       |  - name: blah
-                       |    age: 3000
-                       |    silly: true
-                       |  - name: blah meh
-                       |    age: 13939
-                       |    silly: false
-                       |uuid: 1938484
-                       |isActive: false'''.stripMargin()
-  return new Yaml().load(exampleYaml)
+  def yamlAsString = "a: 1\nb: 2\nc:\n  - aaa\n  - bbb"
+  return new Yaml().load(yamlAsString)
 }
 
 node() {
