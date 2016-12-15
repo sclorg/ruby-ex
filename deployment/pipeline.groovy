c281d65f51dcc124319a540dcf06f21e8cd7870c
Ovidio Mallo <ovidio.mallo@netcetera.com>
2016-12-15 18:07:16 +0100

    buh

--- deployment/pipeline.groovy
+++ deployment/pipeline.groovy
@@ -20,7 +20,7 @@ def parseYaml(content) {
 def ocTemplateParametersAsCommandLineOpt(parameters) {
   def parameterOpts = []
   parameters.each { parameter -> parameterOpts << "-v ${parameter}" }
-  return parameterOpts.join(" ")​​​​
+  return parameterOpts
 }
 
 @NonCPS
