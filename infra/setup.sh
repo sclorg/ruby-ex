d9382370663d115aa65e21347ca20e321c074dac
Ovidio Mallo <ovidio.mallo@gmail.com>
2017-01-02 14:52:48 +0100

    Some cleanup

--- infra/setup.sh
+++ infra/setup.sh
@@ -4,11 +4,22 @@ sudo htpasswd -b /etc/origin/master/htpasswd dev1 dev1
 sudo htpasswd -b /etc/origin/master/htpasswd test1 test1
 
 oc login -u admin -p admin
+oc new-project cicd
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n rubex-dev
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n rubex-test
 
+oc login -u system:admin
+oc project openshift
+oc delete is oc-jenkins
+oc create is oc-jenkins
+oc tag --source=docker --scheduled=true omallo/oc-jenkins:latest openshift/oc-jenkins:latest
+
+oc login -u admin -p admin
 oc new-project rubex-dev
 oc policy add-role-to-user edit dev1 -n rubex-dev
 oc policy add-role-to-user view test1 -n rubex-dev
 
+oc login -u admin -p admin
 oc new-project rubex-test
 oc policy add-role-to-user edit test1 -n rubex-test
 oc policy add-role-to-group system:image-puller system:serviceaccounts:rubex-test -n rubex-dev
@@ -31,18 +42,3 @@ oc login -u dev1 -p dev1
 oc project rubex-dev
 
 oc tag rubex-dev/frontend:latest rubex-dev/frontend:test
-
-oc login -u admin -p admin
-
-oc new-project cicd
-oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n rubex-dev
-oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n rubex-test
-
-oc login -u system:admin
-oc project openshift
-oc delete is oc-jenkins
-oc create is oc-jenkins
-oc tag --source=docker --scheduled=true omallo/oc-jenkins:latest openshift/oc-jenkins:latest
-
-#oc new-app --name=jenkins omallo/oc-jenkins
-#oc expose svc ci --hostname ci.oc.habitz-app.com
