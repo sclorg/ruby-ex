5c4f4a750c6dae44f10e8ff07695fb76bc10fe07
Ovidio Mallo <ovidio.mallo@gmail.com>
2017-01-04 12:53:14 +0100

    Read the config

--- deployment/preview-pipeline.groovy
+++ deployment/preview-pipeline.groovy
@@ -1,6 +1,8 @@
 @Library('occd') _
 
 node() {
+  def config = occd.parseConfig(readFile("deployment/config.yaml"))
+
   stage("Deploy to PREV") {
     occd.tag("rubex-dev", "frontend", "b-${BRANCH}", "prev")
     occd.rollout("rubex-prev", "frontend", config.prev.deployment.frontend)
